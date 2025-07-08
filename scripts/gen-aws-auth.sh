46f45e5e1b122226499620e7755a800e374b91b5
yjking <cohon@naver.com>
2025-07-08 16:00:35 +0900

    drb-test

---
+++ scripts/gen-aws-auth.sh
@@ -0,0 +1,16 @@
+# scripts/gen-aws-auth.sh (리포지토리에 저장)
+cat <<EOF > infra/dr_terraform/envs/aws-auth.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${NODE_ROLE_ARN}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+EOF
+
