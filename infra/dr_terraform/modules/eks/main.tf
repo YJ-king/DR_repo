4e7353304cc3150e01a6f7f3949243012e2c672b
yjking <cohon@naver.com>
2025-07-11 19:11:29 +0900

    addi

--- infra/dr_terraform/modules/eks/main.tf
+++ infra/dr_terraform/modules/eks/main.tf
@@ -31,8 +31,6 @@ resource "aws_eks_node_group" "this" {
   }
   ami_type = "AL2023_x86_64_STANDARD"
 
-  additional_security_group_ids = var.eks_sg_ids
-
   remote_access {
     ec2_ssh_key               = var.ec2_key_pair
     source_security_group_ids = var.eks_sg_ids
