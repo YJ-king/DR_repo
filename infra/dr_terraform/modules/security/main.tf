81a31e055cf692df8fc9fb5c8108119be16d727b
yjking <cohon@naver.com>
2025-07-12 17:36:30 +0900

    ha

--- infra/dr_terraform/modules/security/main.tf
+++ infra/dr_terraform/modules/security/main.tf
@@ -97,12 +97,4 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-resource "aws_security_group_rule" "rds_ingress_from_eks" {
-  type                     = "ingress"
-  from_port               = 3306
-  to_port                 = 3306
-  protocol                = "tcp"
-  security_group_id       = aws_security_group.rds_sg.id
-  source_security_group_id = aws_security_group.eks_node_sg.id
-}
 
